3fb0a82373bf44d8166846802f8391a2b29d5ed9
rebecca-brunsberg <rebeccabrunsberg@unknown-e0-f8-47-15-a7-18.home>
2014-12-02 17:27:53 +0000

    lab08 done
    Racer works differently because when I did first project JumpLeft was
    actually in other direction

--- SteepleChaseRacer.java
+++ SteepleChaseRacer.java
@@ -1,21 +1,35 @@
 /**
  * A SteepleChaseRacer is a Racer that can jump over hurdles of any height.
  * 
- * @author <...>
- * @version <...>
+ * @author Rebecca Brunsberg
+ * @version 2 December 2014
  *
  */
 public class SteepleChaseRacer extends Racer {
-	
-	//TODO Subclasses do not inherit any constructors from their superclass
-	//Consequently, you will need to provide a compatible constructor
-	
-	/**
-	 * Re-implement the method to allow for jumping hurdles of any height
-	 */
-	@Override
-	public void jumpRight() {
-		
-	}
+ 
+  public SteepleChaseRacer(int y){
+    super(y);
+  }
+ 
+  //allows for jumping hurdles of any height
+ @Override
+ public void jumpRight() {
+   turnLeft();
+   while(!rightIsClear()){
+     move();
+   }
+   turnLeft();
+   turnLeft();
+   turnLeft();
+   move();
+   turnLeft();
+   turnLeft();
+   turnLeft();
+   while (frontIsClear()){
+     move();
+   }
+   turnLeft();
+  
+ }
 
 }
